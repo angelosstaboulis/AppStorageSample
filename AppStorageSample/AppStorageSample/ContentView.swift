cb68d4ba0c0b7f707b18435333448220003fb66f
Angelos Staboulis <angelosstaboulis@gmail.com>
2022-12-29 15:31:56 +0200

    Commit minor bug fixes and improvements

--- AppStorageSample/AppStorageSample/ContentView.swift
+++ AppStorageSample/AppStorageSample/ContentView.swift
@@ -12,33 +12,33 @@ struct ContentView: View {
     @AppStorage("counterDecrease") var counterDecrease = 0
    
     var body: some View {
-        Button {
-            counterIncrease = counterIncrease + 1
-            counterDecrease = counterIncrease - 1
-        } label: {
-            Text("AppStorage Sample")
-        }
-        .frame(width: 300, height: 100, alignment: .center)
-        .background(Rectangle().foregroundColor(Color.blue)).foregroundColor(Color.white)
+        VStack{
+            Button {
+                counterIncrease = counterIncrease + 1
+                counterDecrease = counterIncrease - 1
+            } label: {
+                Text("AppStorage Sample")
+            }
             .frame(width: 300, height: 100, alignment: .center)
-            .disabled(true)
-        Spacer()
-        Button {
-            counterIncrease = counterIncrease + 1
-            counterDecrease = counterIncrease - 1
-        } label: {
-            Text("Increase counter = \(counterIncrease)")
-        }
-        .frame(width: 300, height: 100, alignment: .center)
-        .background(Rectangle().foregroundColor(Color.blue)).foregroundColor(Color.white)
+            .background(Rectangle().foregroundColor(Color.blue)).foregroundColor(Color.white)
+                .frame(width: 300, height: 100, alignment: .center)
+                .disabled(true)
+            Spacer()
+            Button {
+                counterIncrease = counterIncrease + 1
+                counterDecrease = counterIncrease - 1
+            } label: {
+                Text("Increase counter = \(counterIncrease)")
+            }
             .frame(width: 300, height: 100, alignment: .center)
-        Button {
-            counterDecrease = counterDecrease - 1
-        } label: {
-            Text("Decrease counter = \(counterDecrease)")
-        }.frame(width: 300, height: 100, alignment: .center).background(Rectangle().foregroundColor(Color.blue)).foregroundColor(Color.white)
-            
-        
+            .background(Rectangle().foregroundColor(Color.blue)).foregroundColor(Color.white)
+                .frame(width: 300, height: 100, alignment: .center)
+            Button {
+                counterDecrease = counterDecrease - 1
+            } label: {
+                Text("Decrease counter = \(counterDecrease)")
+            }.frame(width: 300, height: 100, alignment: .center).background(Rectangle().foregroundColor(Color.blue)).foregroundColor(Color.white)
+        }
     }
 }
 
